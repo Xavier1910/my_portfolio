5ce1ce467d439e733b0d332c0124fcdab90ca1d6
Xavier1910 <akashmsm275@gmail.com>
2025-03-08 15:26:00 +0530

    updated project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,20 @@ import React, { useState, useEffect } from 'react';
 const projects = [
   {
     id: 1,
+    title: 'ShowShe-Instagram Clone',
+    description: 'This is a full-stack Instagram-like web application built using Vite + React for the frontend and Spring Boot with MySQL for the backend. The app allows users to register, log in, create and manage posts, search for users, follow/unfollow users, and view profiles.',
+    imageUrl: [
+      '/images/project/ic1.png',
+      '/images/project/ic2.png',
+      '/images/project/ic3.png',
+      '/images/project/ic4.png',
+      '/images/project/ic5.png',
+      '/images/project/ic6.png',
+    ],
+    githubLink: 'https://github.com/Xavier1910/instagramClone',
+  },
+  {
+    id: 2,
     title: 'Recipe Sharing',
     description: 'A professional platform for sharing, discovering, and refining recipes within a global culinary community.',
     imageUrl: [
@@ -15,20 +29,23 @@ const projects = [
     githubLink: 'https://github.com/Xavier1910/Recipe-Sharing',
   },
   {
-    id: 2,
-    title: 'E-commerce',
-    description: 'A frontend-only e-commerce project offering a seamless, responsive shopping experience with product listings, cart functionality, and checkout pages.',
+    id: 3,
+    title: 'E-commerce Website',
+    description: 'LuxeLane is a full-stack e-commerce web application built using HTML, CSS, JavaScript, Spring Boot, and MySQL. It provides a seamless shopping experience, including product management, user authentication, order processing, and cart functionalities.',
     imageUrl: [
-      '/images/project/ec1.png',
-      '/images/project/ec2.png',
-      '/images/project/ec3.png',
-      '/images/project/ec4.png',
+      '/images/project/ecw1.png',
+      '/images/project/ecw2.png',
+      '/images/project/ecw3.png',
+      '/images/project/ecw4.png',
+      '/images/project/ecw5.png',
+      '/images/project/ecw6.png',
+      '/images/project/ecw7.png',
 
     ],
-    githubLink: 'https://github.com/Xavier1910/e-commerce',
+    githubLink: 'https://github.com/Xavier1910/E-CommerceWebsite',
   },
   {
-    id: 3,
+    id: 4,
     title: 'WhatsApp Clone',
     description: 'A frontend-only WhatsApp clone featuring real-time chat, contact lists, message display, and media sharing, built with modern web technologies.',
     imageUrl: [
@@ -39,6 +56,19 @@ const projects = [
       '/images/project/wa5.png',
     ],
     githubLink: 'https://github.com/yourgithub',
+  }, 
+  {
+    id: 5,
+    title: 'E-commerce',
+    description: 'A frontend-only e-commerce project offering a seamless, responsive shopping experience with product listings, cart functionality, and checkout pages.',
+    imageUrl: [
+      '/images/project/ec1.png',
+      '/images/project/ec2.png',
+      '/images/project/ec3.png',
+      '/images/project/ec4.png',
+
+    ],
+    githubLink: 'https://github.com/Xavier1910/e-commerce',
   },
 ];
 
